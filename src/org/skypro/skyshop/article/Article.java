c8d6cbd7d1297bc9ce8aac63f357f506b201343f
TabrizKakhramanov <tabriz9998@gmil.com>
2025-01-04 20:08:30 +0300

    Made homework

--- src/org/skypro/skyshop/article/Article.java
+++ src/org/skypro/skyshop/article/Article.java
@@ -2,6 +2,8 @@ package org.skypro.skyshop.article;
 
 import org.skypro.skyshop.search.Searchable;
 
+import java.util.Objects;
+
 public class Article implements Searchable {
     private String articleName;
     private String articleContent;
@@ -25,5 +27,18 @@ public class Article implements Searchable {
     @Override
     public String getSearchContentType() {
         return "ARTICLE";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(articleName, article.articleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(articleName);
     }
 }
